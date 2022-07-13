8a255f083ff1ec1011a7d5e412bf9e41dd36768f
Akseli <akseli.laine201@gmail.com>
2022-07-13 06:15:03 +0300

    Added a test

--- PixelSearch.Tests/Program.cs
+++ PixelSearch.Tests/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Drawing;
+﻿#if WINDOWS
+using System.Drawing;
 using System.Drawing.Imaging;
+#endif
 using PixelSearch;
 
 // the bitmap api is only available on windows
@@ -7,10 +9,61 @@ using PixelSearch;
 #if WINDOWS
 static void Test_FindImageOnScreen() {
 
-    const string ImageFile = "Capture.PNG";
+    const string ImageFile = "./resources/Capture.PNG";
 
+    const int ScreenWidth = 1920;
+    const int ScreenHeight = 1080;
+
+
+    // first get the image to search
+    using var imageBmp = new Bitmap(ImageFile);
+
+    // then get the screen pixels
+    // for that, generate an empty bitmap with the screen dimensions
+    using var screenBmp = new Bitmap(ScreenWidth, ScreenHeight);
+    
+    // then create a graphics context from the bitmap
+    using var graphics = Graphics.FromImage(screenBmp);
+
+    // then copy data from the screen over into the generated bitmap
+    graphics.CopyFromScreen(0, 0, 0, 0, new Size(ScreenWidth, ScreenHeight));
+
+
+    // to access the raw pixel data of the bitmaps, they need to be locked
+    var imgData = imageBmp.LockBits(new Rectangle(Point.Empty, imageBmp.Size), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+    var screenData = screenBmp.LockBits(new Rectangle(Point.Empty, screenBmp.Size), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+
+    // now that we have access to the pixel data, we can finally perform the pixel search
+
+    if (FastSearch.FindPixels(
+            needle: imgData.Scan0, // the needle is the pixel data of the image to be searched
+            imgWidth: imageBmp.Width,
+            imgHeight: imageBmp.Height,
+            
+            haystack: screenData.Scan0, // the haystack is the pixel data where the image will be searched from, in this case the screen
+            haystackWidth: screenBmp.Width, // we also need to input the haystack width
+
+            // clip rect is the area where the image will be searched from inside the screen
+            clipX: 0,
+            clipY: 0,
+            clipWidth: ScreenWidth,
+            clipHeight: ScreenHeight,
+
+            out var location // finally the location where the image was found
+    )) {
+        Console.WriteLine($"Image was found at (x: {location.x}, y: {location.y})");
+    } else {
+        Console.WriteLine("The image could not be found from the screen.");
+    }
+
+
+    // its important to unlock the memory after the operation
+    imageBmp.UnlockBits(imgData);
+    screenBmp.UnlockBits(screenData);
 }
 
-
 Test_FindImageOnScreen();
+#else
+Console.WriteLine("This test is only available on Windows");
 #endif
--- PixelSearch/FastSearch.cs
+++ PixelSearch/FastSearch.cs
@@ -19,6 +19,7 @@ public static class FastSearch {
     /// <param name="imgWidth">The needle image width</param>
     /// <param name="imgHeight">The needle image height</param>
     /// <param name="haystack">Pointer to the begging of the haystack pixels to be searched</param>
+    /// <param name="haystackWidth">Width of the haystack</param>
     /// <param name="clipX">X coordinate of the clip rect</param>
     /// <param name="clipY">Y coordinate of the clip rect</param>
     /// <param name="clipWidth">Width of the clip rect</param>
@@ -26,11 +27,13 @@ public static class FastSearch {
     /// <param name="location">Location of the needle, if it was found; otherwise (-1, -1)</param>
     /// <returns><c>true</c> if the needle pixels were found from the haystack; otherwise, <c>false</c></returns>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public static bool FindPixels(IntPtr needle, int imgWidth, int imgHeight, IntPtr haystack, int rowLength, int clipX, int clipY, int clipWidth, int clipHeight, out (int x, int y) location) {
+    public static bool FindPixels(IntPtr needle, int imgWidth, int imgHeight, IntPtr haystack, int haystackWidth, int clipX, int clipY, int clipWidth, int clipHeight, out (int x, int y) location) {
 
         if (clipWidth < imgWidth || clipHeight < imgHeight) {
             throw new ArgumentOutOfRangeException("clip width and height must be within image dimensions");
         }
+
+
 
         // calculate the last possible position of the clip
         // where the needle image can fit
@@ -42,6 +45,7 @@ public static class FastSearch {
 
         unsafe {
 
+            
             // get the pointers to the img and haystack
             int* imgPtr = (int*)needle;
             int* haystackPtr = (int*)haystack;
@@ -58,22 +62,22 @@ public static class FastSearch {
 
                     // check if any pixels in the current position vector match the first pixel of the image
 
-                    if (!Vector.EqualsAny(firstPixelScalar, *(Vector<int>*)(haystackPtr + currentX + currentY * rowLength))) {
+                    if (!Vector.EqualsAny(firstPixelScalar, *(Vector<int>*)(haystackPtr + currentX + currentY * haystackWidth))) {
                         continue;
                     }
 
                     // loop through the positions and check if every pixel matches with the image
                     for (int index = 0; index < Vector<int>.Count; index++) {
-                        if (MatchAllPixels(imgPtr, haystackPtr + (currentX + index + currentY * rowLength), imgWidth, imgHeight, rowLength)) {
+                        if (MatchAllPixels(imgPtr, haystackPtr + (currentX + index + currentY * haystackWidth), imgWidth, imgHeight, haystackWidth)) {
                             location = (currentX + index, currentY);
                             return true;
                         }
                     }
                 }
-
+                
                 // the rest of the pixels in the row will be matched one by one
                 for (; currentX < endX; currentX++) {
-                    if (MatchAllPixels(imgPtr, haystackPtr + (currentX + currentY * rowLength), imgWidth, imgHeight, rowLength)) {
+                    if (MatchAllPixels(imgPtr, haystackPtr + (currentX + currentY * haystackWidth), imgWidth, imgHeight, haystackWidth)) {
                         location = (currentX, currentY);
                         return true;
                     }
@@ -92,7 +96,7 @@ public static class FastSearch {
     /// </summary>
     /// <returns><c>true</c> if all pixels match; otherwise, <c>false</c></returns>
     unsafe private static bool MatchAllPixels(int* needlePtr, int* haystackRegion, int width, int height, int rowLength) {
-
+     
         // loop through the pixels to find
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
